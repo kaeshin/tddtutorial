c61164e3b518105d14a20ddf57f91e5d2701aa26
kaeshin <kaeshin.yong@seekasia.com>
2017-09-02 13:43:12 +0800

    My first TDD

--- functional_test.py
+++ functional_test.py
@@ -1,6 +1,19 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
+    
+    def setUp(self):
+        self.browser = webdriver.Firefox()
 
-assert 'Django' in browser.title
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        self.browser.get('http://localhost:8000')
+
+        # She notices the page title and header mention to-do lists
+        self.assertIn('Django', self.browser.title)
+
+if __name__ == '__main__':
+    unittest.main()
